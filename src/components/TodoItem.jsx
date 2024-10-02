f75056294b547a3847fde55f78d31f2eec996769
Mukesh Kumar <mkso13189@gmail.com>
2024-10-02 13:30:42 +0530

    Add deleteItem and editItem functionality to todoItems, and improve namespace for variables and event handlers

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,3 +1,34 @@
-export default function TodoItem({ name }) {
-  return <li>{name}</li>;
+import { useState } from "react";
+
+export default function TodoItem({ task, handleDelete, handleEdit }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(task.title);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleEdit(task.id, editedTitle);
+    setIsEditing(false);
+  }
+
+  return (
+    <li>
+      {isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)}
+          />
+          <button type="submit">Save</button>
+          <button onClick={() => setIsEditing(false)}>Cancel</button>
+        </form>
+      ) : (
+        <>
+          {task.title}
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+          <button onClick={() => handleDelete(task.id)}>Delete</button>
+        </>
+      )}
+    </li>
+  );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,36 @@
 import { useState } from "react";
 import TodoItem from "./TodoItem";
 
+const TODOS = [
+  { id: 0, title: "Start building Planero", completed: true },
+  { id: 1, title: "Stop procrastinating", completed: false },
+  { id: 2, title: "You can't learn unless you build", completed: true },
+];
+
 export default function TodoList() {
-  const [items, setItems] = useState([
-    { id: 0, name: "Start building Planero" },
-    { id: 1, name: "Stop procrastinating" },
-    { id: 2, name: "You can't learn unless you build" },
-  ]);
-  console.log(items);
+  const [tasks, setTasks] = useState(TODOS);
+
+  function deleteTask(id) {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+  function editTask(id, updatedTitle) {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, title: updatedTitle } : task,
+      ),
+    );
+  }
+
   return (
     <ul>
-      {items.map((item) => (
-        <TodoItem key={item.id} name={item.name} />
+      {tasks.map((task, index) => (
+        <TodoItem
+          key={index}
+          id={index}
+          task={task}
+          handleDelete={deleteTask}
+          handleEdit={editTask}
+        />
       ))}
     </ul>
   );
