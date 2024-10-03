527d4425dcdbf271d57f46b4db2c2ba1ba8d3fe0
Mukesh Kumar <mkso13189@gmail.com>
2024-10-03 20:03:38 +0530

    Add CSS classes on site heading

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import TodoList from "./components/TodoList.jsx";
 export default function App() {
   return (
     <>
-      <h1>Planero</h1>
+      <h1 className="text-2xl font-bold text-center mb-4">
+        Planero project manager
+      </h1>
       <ul>
         <TodoList />
       </ul>
