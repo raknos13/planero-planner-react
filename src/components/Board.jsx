976afb4f79d636c1dab48eb1bdfdda69cafe4664
Mukesh Kumar <mkso13189@gmail.com>
2024-10-21 14:51:58 +0530

    Render multiple list in board component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,9 +2,10 @@ import TodoList from "./TodoList";
 
 export default function Board() {
   return (
-    <div className="flex">
-      <TodoList />
-      {/* <TodoList /> */}
+    <div className="flex justify-start w-auto h-screen mx-3 my-3">
+      <TodoList title="Todo" />
+      <TodoList title="Todo" />
+      <TodoList title="Todo" />
     </div>
   );
 }
