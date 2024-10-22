e97d352d4655e56ab4e49fef97aebd358d21e35d
Mukesh Kumar <mkso13189@gmail.com>
2024-10-22 16:00:03 +0530

    Take in listId as prop for task functions

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,19 @@
 import { useState, useRef, useEffect } from "react";
-import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
+import { Draggable, Droppable } from "@hello-pangea/dnd";
 import { AiOutlinePlus, AiOutlineClose } from "react-icons/ai";
-// import { v4 as uuidv4 } from "uuid";
-import TodoItem from "./TodoItem";
+import ListItem from "./TodoItem";
+import { v4 as uuidv4 } from "uuid";
 import { useTasks } from "../hooks/useTasks";
 
-export default function TodoList({ initialTasks, title, onDragEnd }) {
-  const { tasks, addTask, deleteTask, editTask } = useTasks(initialTasks);
-  const [newTask, setNewTask] = useState("");
+export default function TodoList({
+  list,
+  addTask,
+  editTask,
+  deleteTask,
+  dragHandleProps,
+}) {
+  const [newTask, setNewTask] = useState({});
   const [isAddingNewTask, setIsAddingNewTask] = useState(false);
-
   const textareaRef = useRef(null);
 
   useEffect(() => {
@@ -20,54 +24,60 @@ export default function TodoList({ initialTasks, title, onDragEnd }) {
 
   function handleSubmit(e) {
     e.preventDefault();
-    addTask(newTask);
-    setNewTask("");
+    addTask(list.id, newTask);
+    setNewTask({});
     // setIsAddingNewTask(false);
   }
 
   return (
     <div className="container mx-2 p-2 bg-gray-200 rounded-xl w-[300px] h-min">
-      <div className="font-bold mb-2 p-1">
-        <span>{title}</span>
+      <div {...dragHandleProps}>
+        <div className="font-bold mb-2 p-1">
+          <span>{list.title}</span>
+        </div>
       </div>
-      {/* Wrap drag-and-drop context */}
-      <DragDropContext onDragEnd={onDragEnd}>
-        {/* Define droppable area */}
-        <Droppable droppableId="droppable">
-          {(provided) => (
-            <ul
-              // pass necessary props to make the list droppable
-              {...provided.droppableProps}
-              // bind ref to DOM element
-              ref={provided.innerRef}
-            >
-              {tasks &&
-                tasks.map((task, index) => (
-                  <Draggable key={task.id} draggableId={task.id} index={index}>
-                    {(provided) => (
-                      <TodoItem
-                        ref={provided.innerRef}
-                        draggableProps={provided.draggableProps}
-                        dragHandleProps={provided.dragHandleProps}
-                        // key={index}
-                        task={task}
-                        handleEdit={editTask}
-                        handleDelete={deleteTask}
-                      />
-                    )}
-                  </Draggable>
-                ))}
-              {provided.placeholder}
-            </ul>
-          )}
-        </Droppable>
-      </DragDropContext>
+      <Droppable droppableId="droppable">
+        {(provided) => (
+          <ul
+            // pass necessary props to make the list droppable
+            {...provided.droppableProps}
+            // bind ref to DOM element
+            ref={provided.innerRef}
+          >
+            {list.tasks &&
+              list.tasks.map((task, index) => (
+                <Draggable key={task.id} draggableId={task.id} index={index}>
+                  {(provided) => (
+                    <listItem
+                      ref={provided.innerRef}
+                      draggableProps={provided.draggableProps}
+                      dragHandleProps={provided.dragHandleProps}
+                      // key={index}
+                      listId={list.id}
+                      task={task}
+                      handleEdit={editTask}
+                      handleDelete={deleteTask}
+                    />
+                  )}
+                </Draggable>
+              ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+
       {isAddingNewTask && (
         <textarea
           placeholder="Type a task to add..."
-          value={newTask}
+          value={newTask.title}
           ref={textareaRef}
-          onChange={(e) => setNewTask(e.target.value)}
+          onChange={(e) =>
+            setNewTask({
+              id: uuidv4(),
+              title: e.target.value,
+              completed: false,
+            })
+          }
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               handleSubmit(e);
@@ -75,11 +85,12 @@ export default function TodoList({ initialTasks, title, onDragEnd }) {
           }}
           onBlur={() => {
             setIsAddingNewTask(false);
-            setNewTask("");
+            setNewTask({});
           }}
           className="mb-1 p-2 w-full rounded-lg text-sm"
         />
       )}
+
       <div className="flex">
         <button
           // type="submit"
