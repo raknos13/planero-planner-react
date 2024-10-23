369a13b8827d898754600a3cde2da3c0425a8256
Mukesh Kumar <mkso13189@gmail.com>
2024-10-23 17:03:38 +0530

    Change state variable acc to new data structure

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,32 +1,49 @@
 import { useState, useRef, useEffect } from "react";
 import { Draggable, Droppable } from "@hello-pangea/dnd";
 import { AiOutlinePlus, AiOutlineClose } from "react-icons/ai";
-import ListItem from "./TodoItem";
+import Card from "./Card";
 import { v4 as uuidv4 } from "uuid";
-import { useTasks } from "../hooks/useTasks";
 
-export default function TodoList({
+export default function List({
   list,
-  addTask,
-  editTask,
-  deleteTask,
+  cards,
+  deleteCard,
+  addCard,
+  editCard,
   dragHandleProps,
 }) {
-  const [newTask, setNewTask] = useState({});
-  const [isAddingNewTask, setIsAddingNewTask] = useState(false);
+  const cardTemplate = {
+    id: "Add a unique ID",
+    title: "",
+    description: "Add a description",
+    labels: "",
+    completed: false,
+  };
+  const [newCard, setNewCard] = useState(cardTemplate);
+  const [isAddingNewCard, setIsAddingNewCard] = useState(false);
   const textareaRef = useRef(null);
 
   useEffect(() => {
-    if (textareaRef.current && isAddingNewTask) {
+    if (textareaRef.current && isAddingNewCard) {
       textareaRef.current.focus();
     }
-  }, [isAddingNewTask]);
+  }, [isAddingNewCard]);
 
   function handleSubmit(e) {
     e.preventDefault();
-    addTask(list.id, newTask);
-    setNewTask({});
-    // setIsAddingNewTask(false);
+    addCard(list.id, newCard);
+    setNewCard(cardTemplate);
+    // setIsAddingNewCard(false);
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter" && !e.shiftKey) {
+      handleSubmit(e);
+    }
+    if (e.key === "Escape") {
+      setIsAddingNewCard(false);
+      setNewCard(cardTemplate);
+    }
   }
 
   return (
@@ -44,48 +61,42 @@ export default function TodoList({
             // bind ref to DOM element
             ref={provided.innerRef}
           >
-            {list.cards &&
-              list.cards.map((card, index) => (
-                <Draggable key={card.id} draggableId={card.id} index={index}>
-                  {(provided) => (
-                    <ListItem
-                      ref={provided.innerRef}
-                      draggableProps={provided.draggableProps}
-                      dragHandleProps={provided.dragHandleProps}
-                      // key={index}
-                      listId={list.id}
-                      card={card}
-                      handleEdit={editTask}
-                      handleDelete={deleteTask}
-                    />
-                  )}
-                </Draggable>
-              ))}
+            {cards.map((card, index) => (
+              <Draggable key={card.id} draggableId={card.id} index={index}>
+                {(provided) => (
+                  <Card
+                    ref={provided.innerRef}
+                    draggableProps={provided.draggableProps}
+                    dragHandleProps={provided.dragHandleProps}
+                    listId={list.id}
+                    card={card}
+                    handleEdit={editCard}
+                    handleDelete={deleteCard}
+                  />
+                )}
+              </Draggable>
+            ))}
             {provided.placeholder}
           </ul>
         )}
       </Droppable>
 
-      {isAddingNewTask && (
+      {isAddingNewCard && (
         <textarea
-          placeholder="Type a card to add..."
-          value={newTask.title}
+          placeholder="Enter a title..."
+          value={newCard.title}
           ref={textareaRef}
           onChange={(e) =>
-            setNewTask({
+            setNewCard({
+              ...newCard,
               id: uuidv4(),
               title: e.target.value,
-              completed: false,
             })
           }
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              handleSubmit(e);
-            }
-          }}
+          onKeyDown={handleKeyDown}
           onBlur={() => {
-            setIsAddingNewTask(false);
-            setNewTask({});
+            setIsAddingNewCard(false);
+            setNewCard(cardTemplate);
           }}
           className="mb-1 p-2 w-full rounded-lg text-sm"
         />
@@ -93,18 +104,15 @@ export default function TodoList({
 
       <div className="flex">
         <button
-          // type="submit"
-          onClick={() => {
-            setIsAddingNewTask(true);
-          }}
+          onClick={() => setIsAddingNewCard(true)}
           className="flex justify-start cards-center gap-2 p-2 bg-gray-200 rounded-md w-full text-sm hover:bg-gray-400"
         >
           <AiOutlinePlus />
           <span>Add new card</span>
         </button>
-        {isAddingNewTask && (
+        {isAddingNewCard && (
           <button
-            onClick={() => setIsAddingNewTask(false)}
+            onClick={() => setIsAddingNewCard(false)}
             className="p-1 px-2 rounded-md hover:bg-gray-400"
           >
             <AiOutlineClose />
