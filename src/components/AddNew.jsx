279133202dcd84869440ce38fa60994580ecce69
Mukesh Kumar <mkso13189@gmail.com>
2024-10-24 18:50:08 +0530

    disable escape handling, race cond. w. onBlur
    Disable addNew on onBlur for now, onBlur cancels adding new, and
    disabled all escape handling for canceling task as it is not getting
    recognized because of onBlur. Added TODOS for later to implement this

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -5,34 +5,95 @@ const AddNew = ({ type, handleAddNew }) => {
   const [isAddingNew, setIsAddingNew] = useState(false);
   const [newTitle, setNewTitle] = useState("");
   const inputRef = useRef(null);
-  const isClickingAddButton = useRef(false);
+  const hasClickedAddButton = useRef(false);
+  const hasPressedEscape = useRef(false);
 
   useEffect(() => {
     if (inputRef.current && isAddingNew) {
+      // ensure focus happens after DOM has rendered textarea
       const timeoutFocus = setTimeout(() => {
         inputRef.current.focus();
       }, 0);
       return () => clearTimeout(timeoutFocus);
     }
-  }, [isAddingNew, newTitle]);
+  }, [isAddingNew]);
 
-  function handleSubmit() {
-    if (newTitle.trim()) {
-      handleAddNew(newTitle);
-      setNewTitle("");
+  // useEffect(() => {
+  //   const handleGlobalKeyDown = (e) => {
+  //     if (e.key === "Escape") {
+  //       // e.preventDefault();
+  //       hasPressedEscape.current = true;
+  //       handleCancel();
+  //     }
+  //   };
+  //
+  //   window.addEventListener("keydown", handleGlobalKeyDown);
+  //
+  //   return () => {
+  //     window.removeEventListener("keydown", handleGlobalKeyDown);
+  //   };
+  // }, []);
+
+  function handleClick() {
+    if (!isAddingNew) {
+      // handle initial click to open textarea
+      setIsAddingNew(true);
+    } else {
+      // subsequent clicks when textarea is open
+      if (newTitle.trim()) {
+        handleAddNew(newTitle);
+        setNewTitle("");
+      }
+      // Keep textarea focused after submission
+      inputRef.current?.focus();
     }
-    setIsAddingNew(true);
+    // set to true onMouseDown event
+    hasClickedAddButton.current = false; //reset after handling click
+  }
+
+  function handleCancel() {
+    setIsAddingNew(false);
+    setNewTitle("");
   }
 
   function handleKeyDown(e) {
     if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault();
-      handleSubmit();
+      if (newTitle.trim()) {
+        handleAddNew(newTitle);
+        setNewTitle("");
+      }
+      setIsAddingNew(true);
     }
-    if (e.key === "Escape") {
-      setIsAddingNew(false);
-      setNewTitle("");
-    }
+    // else if (e.key === "Escape") {
+    // // e.preventDefault();
+    // hasPressedEscape.current = true;
+    // handleCancel();
+    // // inputRef.current?.blur(); // Ensure focus leaves the textarea
+    // }
+  }
+
+  function handleBlur() {
+    // Only handle blur if we're not clicking the add button
+    // don't add new if blur triggerd by pressing Escape
+
+    // TODO: because pressing escape is triggering onBlur and not onKeyDown,
+    // TODO: I'm disabling adding task on blur, and blur cancels
+    // TODO: adding task, later implement escape to cancel task and blur adds new task if textarea not empty
+
+    console.log(hasPressedEscape.current);
+    // if (hasPressedEscape.current) {
+    //   setTimeout(() => {
+    //     hasPressedEscape.current = false;
+    //   }, 0);
+    //   return;
+    // }
+    // if (!hasClickedAddButton.current && newTitle.trim()) {
+    //   handleAddNew(newTitle);
+    //   setNewTitle("");
+    // }
+    // setIsAddingNew(false);
+    handleCancel();
   }
 
   return (
@@ -44,27 +105,17 @@ const AddNew = ({ type, handleAddNew }) => {
           ref={inputRef}
           onChange={(e) => setNewTitle(e.target.value)}
           onKeyDown={handleKeyDown}
-          onBlur={() => {
-            if (!newTitle.trim() && !isClickingAddButton.current) {
-              setIsAddingNew(false);
-              setNewTitle("");
-            }
-          }}
-          className="mb-1 p-2 w-full rounded-lg text-sm"
+          onBlur={handleBlur}
+          className="mb-1 p-2 px-3 w-full rounded-lg text-sm"
         />
       )}
 
       <div className="flex">
         <button
           onMouseDown={() => {
-            isClickingAddButton.current = true;
+            hasClickedAddButton.current = true;
           }}
-          onClick={() => {
-            setIsAddingNew(true);
-            handleSubmit();
-            inputRef.current?.focus();
-            isClickingAddButton.current = false; //reset after handling click
-          }}
+          onClick={handleClick}
           className="flex justify-start items-center gap-2 p-2 bg-gray-200 rounded-md w-full text-sm hover:bg-gray-400"
         >
           <AiOutlinePlus />
@@ -72,10 +123,7 @@ const AddNew = ({ type, handleAddNew }) => {
         </button>
         {isAddingNew && (
           <button
-            onClick={() => {
-              setIsAddingNew(false);
-              setNewTitle("");
-            }}
+            onClick={handleCancel}
             className="p-1 px-2 rounded-md hover:bg-gray-400"
           >
             <AiOutlineClose />
