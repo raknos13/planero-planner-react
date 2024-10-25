71c8d01268b1c21a4b411485ef3f91bb47523dc6
Mukesh Kumar <mkso13189@gmail.com>
2024-10-25 16:24:50 +0530

    Render the sidebar component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Board from "./components/Board.jsx";
+import Sidebar from "./components/Sidebar.jsx";
 
 export default function App() {
   return (
-    <>
-      <h1 className="text-2xl font-bold text-center mb-4">
-        Planero project manager
-      </h1>
-      <ul>
-        <Board />
-      </ul>
-    </>
+    <div className="h-screen w-screen flex">
+      <Sidebar />
+      <div>
+        <h1 className="text-2xl font-bold text-center mb-4">
+          Planero project manager
+        </h1>
+        <div className="flex-1">
+          <Board />
+        </div>
+      </div>
+    </div>
   );
 }
