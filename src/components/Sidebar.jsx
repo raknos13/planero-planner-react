fc7df2ac22f5e4a76ecc42cb2539c69f9029ad4c
Mukesh Kumar <mkso13189@gmail.com>
2024-10-25 18:17:13 +0530

    Replace icons w fi icons and tweak sidebar styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,33 +1,32 @@
-import { AiOutlinePlus } from "react-icons/ai";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { FiChevronLeft, FiChevronRight, FiPlus } from "react-icons/fi";
 import { useState } from "react";
 
 const Sidebar = () => {
-  const [isCollapsed, setIsCollapsed] = useState(0);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
   return (
     <div
       className={`h-screen transition-all ease-linear duration-200 ${isCollapsed ? "w-10" : "w-52"} flex-shrink-0 bg-gray-200`}
     >
       {!isCollapsed && (
-        <div className="flex justify-between align-center p-2">
-          <span className="font-bold">Your boards</span>
-          <div>
-            <AiOutlinePlus />
-          </div>
+        <div className="flex justify-between align-center p-3 ">
+          <h1 className="font-bold text-sm">User's workspace</h1>
           <button
             onClick={() => setIsCollapsed(!isCollapsed)}
-            className="hover:bg-gray-400 p-1 m-1 rounded-md"
+            className="hover:bg-gray-400 p-1 rounded-md"
           >
-            <FiChevronLeft />
+            <FiChevronLeft size={18} />
           </button>
+          {/* <div> */}
+          {/*   <FiPlus /> */}
+          {/* </div> */}
         </div>
       )}
       {isCollapsed && (
-        <div>
+        <div className="m-2">
           <button
             onClick={() => setIsCollapsed(!isCollapsed)}
-            className="hover:bg-gray-400 p-1 m-1 rounded-md"
+            className="hover:bg-gray-400 p-1 rounded-md"
           >
             <FiChevronRight />
           </button>
