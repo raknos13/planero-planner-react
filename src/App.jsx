a4e7e770deacd83b51a6b5b24924629193e0bdd9
Mukesh Kumar <mkso13189@gmail.com>
2024-10-27 13:31:10 +0530

    Move entry point to Main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import Board from "./components/Board.jsx";
-import Sidebar from "./components/Sidebar.jsx";
+import Main from "./components/Main";
 
 export default function App() {
-  return (
-    <div className="h-screen w-screen">
-      <h1 className="text-md font-bold text-start p-4 bg-gray-200">
-        Planero project manager
-      </h1>
-      <div className="flex">
-        <Sidebar />
-        <div className="flex-1 mt-2">
-          <Board />
-        </div>
-      </div>
-    </div>
-  );
+  return <Main />;
 }
