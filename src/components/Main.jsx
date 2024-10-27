8caefb8068194149fab99685507dc776dbd1d7f0
Mukesh Kumar <mkso13189@gmail.com>
2024-10-27 15:25:48 +0530

    Extract header logic into a separate component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,11 @@
 import Board from "./Board.jsx";
 import Sidebar from "./Sidebar.jsx";
+import Header from "./Header.jsx";
 
 export default function Main() {
   return (
     <div className="h-screen w-screen">
-      <h1 className="text-md font-bold text-start p-4 bg-gray-200">
-        Planero project manager
-      </h1>
+      <Header />
       <div className="flex">
         <Sidebar />
         <div className="flex-1 mt-2">
