ed8c49007be27791f712cd3f5471d1152962c11c
Mukesh Kumar <mkso13189@gmail.com>
2024-10-28 23:43:59 +0530

    Handle data preparation for active board

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,34 @@
 import Board from "./Board.jsx";
 import Sidebar from "./Sidebar.jsx";
 import Header from "./Header.jsx";
+import { useBoardContext } from "./BoardContext.jsx";
 
 export default function Main() {
+  const { boards, activeBoardId, lists, cards } = useBoardContext();
+
+  const activeBoard = boards[activeBoardId];
+
+  const activeBoardLists = activeBoard.listIds.reduce((acc, listId) => {
+    acc[listId] = lists[listId];
+    return acc;
+  }, {});
+
+  const activeBoardCards = Object.values(lists).flatMap((list) =>
+    list.cardIds.map((cardId) => cards[cardId]),
+  );
+
+  const boardData = {
+    lists: activeBoardLists,
+    cards: activeBoardCards,
+    listOrder: activeBoard.listOrder,
+  };
+
   return (
     <div className="h-screen w-screen overflow-hidden flex flex-col">
       <Header />
       <div className="flex flex-grow">
-          <Sidebar />
-          <Board />
+        <Sidebar />
+        <Board initialData={boardData} />
       </div>
     </div>
   );
