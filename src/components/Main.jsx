5fe30f0ce9e847fe80305a9f19502b445a43cc70
Mukesh Kumar <mkso13189@gmail.com>
2024-10-30 15:13:39 +0530

    Remove unused code and Wrap jsx in BoardProvider

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,35 +1,18 @@
 import Board from "./Board.jsx";
 import Sidebar from "./Sidebar.jsx";
 import Header from "./Header.jsx";
-import { useBoardContext } from "./BoardContext.jsx";
+import { BoardProvider } from "./BoardContext";
 
 export default function Main() {
-  const { boards, activeBoardId, lists, cards } = useBoardContext();
-
-  const activeBoard = boards[activeBoardId];
-
-  const activeBoardLists = activeBoard.listIds.reduce((acc, listId) => {
-    acc[listId] = lists[listId];
-    return acc;
-  }, {});
-
-  const activeBoardCards = Object.values(lists).flatMap((list) =>
-    list.cardIds.map((cardId) => cards[cardId]),
-  );
-
-  const boardData = {
-    lists: activeBoardLists,
-    cards: activeBoardCards,
-    listOrder: activeBoard.listOrder,
-  };
-
   return (
-    <div className="h-screen w-screen overflow-hidden flex flex-col">
-      <Header />
-      <div className="flex flex-grow">
-        <Sidebar />
-        <Board boardData={boardData} />
+    <BoardProvider>
+      <div className="h-screen w-screen overflow-hidden flex flex-col">
+        <Header />
+        <div className="flex flex-grow">
+          <Sidebar />
+          <Board />
+        </div>
       </div>
-    </div>
+    </BoardProvider>
   );
 }
