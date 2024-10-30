e91cb0402dec83898b4c2ae502d01dda8c4b8c30
Mukesh Kumar <mkso13189@gmail.com>
2024-10-31 00:40:06 +0530

    Tweak list css classes

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -16,44 +16,37 @@ export default function Board({ boardData }) {
           <div
             {...provided.droppableProps}
             ref={provided.innerRef}
-            // className="flex h-100 justify-start overflow-x-auto whitespace-nowrap scroll-smooth snap-x snap-mandatory  mx-4 my-4"
-            className="boardContainer flex flex-grow w-full h-full p-4 overflow-x-scroll"
+            className="boardContainer flex p-4 overflow-x-scroll"
           >
-            <div>
-              <div className="listContainer flex gap-3 h-min">
-                {activeBoard.listIds.map((listId, index) => {
-                  const list = lists[listId];
-                  const listCards = list.cardIds.map((cardId) => cards[cardId]);
-                  return (
-                    <Draggable
-                      key={list.id}
-                      draggableId={list.id}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          <List
-                            list={list}
-                            listCards={listCards}
-                            dragHandleProps={provided.dragHandleProps}
-                          />
-                        </div>
-                      )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-                <div className="min-w-[250px] bg-gray-200 h-min rounded-xl p-2 mx-2 mr-4">
-                  <AddNew
-                    type="list"
-                    multiAddMode={false}
-                    handleAddNew={addNewList}
-                  />
-                </div>
+            <div className="listContainer flex gap-3">
+              {activeBoard.listIds.map((listId, index) => {
+                const list = lists[listId];
+                const listCards = list.cardIds.map((cardId) => cards[cardId]);
+                return (
+                  <Draggable key={list.id} draggableId={list.id} index={index}>
+                    {(provided) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <List
+                          list={list}
+                          listCards={listCards}
+                          dragHandleProps={provided.dragHandleProps}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+              <div className="min-w-64 bg-gray-200 h-min rounded-lg p-2">
+                <AddNew
+                  type="list"
+                  multiAddMode={false}
+                  handleAddNew={addNewList}
+                />
               </div>
             </div>
           </div>
