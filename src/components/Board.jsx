8d810d25e445dad12fc8feab4b9b4bfb95537427
Mukesh Kumar <mkso13189@gmail.com>
2024-10-30 13:45:21 +0530

    Update board component w.r.t. Board Context

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,126 +3,21 @@ import { v4 as uuidv4 } from "uuid";
 import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import List from "./List";
 import AddNew from "./AddNew";
-import { initialData } from "../initialData";
+import { useBoardContext } from "./BoardContext";
 
-export default function Board() {
-  const [data, setData] = useState(initialData);
+export default function Board({ boardData }) {
+  const {
+    boards,
+    lists,
+    cards,
+    activeBoardId,
+    addNewList,
+    deleteList,
+    onDragEnd,
+  } = useBoardContext();
+  // const [data, setData] = useState(boardData);
 
-  function addList(title) {
-    const newListId = `list-${uuidv4()}`;
-    const newList = {
-      id: newListId,
-      title: title,
-      cardIds: [],
-    };
-    setData({
-      ...data,
-      lists: {
-        ...data.lists,
-        [newListId]: newList,
-      },
-      listOrder: [...data.listOrder, newListId],
-    });
-  }
-
-  function addCard(listId, newCard) {
-    // if (newCard.title.trim() === "") return;
-    const list = data.lists[listId];
-    const updatedCardIds = [...list.cardIds, newCard.id];
-    setData({
-      ...data,
-      lists: {
-        ...data.lists,
-        [listId]: { ...list, cardIds: updatedCardIds },
-      },
-      cards: { ...data.cards, [newCard.id]: newCard },
-    });
-  }
-
-  function deleteCard(listId, cardId) {
-    const list = data.lists[listId];
-    const newCardIds = list.cardIds.filter((id) => id !== cardId);
-    const { [cardId]: deletedCard, ...remainingCards } = data.cards;
-    setData({
-      ...data,
-      lists: {
-        ...data.lists,
-        [listId]: { ...list, cardIds: newCardIds },
-      },
-      cards: remainingCards,
-    });
-  }
-
-  function editCard(cardId, updatedCard) {
-    const updatedCards = { ...data.cards, [cardId]: updatedCard };
-    setData({
-      ...data,
-      cards: updatedCards,
-    });
-  }
-
-  // Function to handle what happens when dragging ends
-  function onDragEnd(result) {
-    const { destination, source, type } = result;
-    // If there's no destination (dropped outside), do nothing
-    if (!destination) return;
-
-    // Handle list reordering
-    if (type === "list") {
-      const newListOrder = Array.from(data.listOrder);
-      const [movedList] = newListOrder.splice(source.index, 1);
-      newListOrder.splice(destination.index, 0, movedList);
-      setData({
-        ...data,
-        listOrder: newListOrder,
-      });
-      return;
-    }
-
-    // Handle card reordering
-    const sourceList = data.lists[source.droppableId];
-    const destList = data.lists[destination.droppableId];
-
-    // If moving to same list
-    if (sourceList === destList) {
-      const newCardIds = Array.from(sourceList.cardIds);
-      const [movedCard] = newCardIds.splice(source.index, 1);
-      newCardIds.splice(destination.index, 0, movedCard);
-
-      setData({
-        ...data,
-        lists: {
-          ...data.lists,
-          [sourceList.id]: {
-            ...sourceList,
-            cardIds: newCardIds,
-          },
-        },
-      });
-    } else {
-      // If moving to different list
-      const sourceCardIds = Array.from(sourceList.cardIds);
-      const destCardIds = Array.from(destList.cardIds);
-
-      const [movedCard] = sourceCardIds.splice(source.index, 1);
-      destCardIds.splice(destination.index, 0, movedCard);
-
-      setData({
-        ...data,
-        lists: {
-          ...data.lists,
-          [sourceList.id]: {
-            ...sourceList,
-            cardIds: sourceCardIds,
-          },
-          [destList.id]: {
-            ...destList,
-            cardIds: destCardIds,
-          },
-        },
-      });
-    }
-  }
+  const activeBoard = boards[activeBoardId];
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
@@ -136,11 +31,9 @@ export default function Board() {
           >
             <div>
               <div className="listContainer flex gap-3 h-min">
-                {data.listOrder.map((listId, index) => {
-                  const list = data.lists[listId];
-                  const cards = list.cardIds.map(
-                    (cardId) => data.cards[cardId],
-                  );
+                {activeBoard.listOrder.map((listId, index) => {
+                  const list = lists[listId];
+                  const listCards = list.cardIds.map((cardId) => cards[cardId]);
                   return (
                     <Draggable
                       key={list.id}
@@ -154,11 +47,8 @@ export default function Board() {
                           {...provided.dragHandleProps}
                         >
                           <List
-                            list={list}
-                            cards={cards}
-                            deleteCard={deleteCard}
-                            editCard={editCard}
-                            addCard={addCard}
+                            // list={list}
+                            // cards={cards}
                             dragHandleProps={provided.dragHandleProps}
                           />
                         </div>
@@ -171,7 +61,7 @@ export default function Board() {
                   <AddNew
                     type="list"
                     multiAddMode={false}
-                    handleAddNew={addList}
+                    handleAddNew={addNewList}
                   />
                 </div>
               </div>
