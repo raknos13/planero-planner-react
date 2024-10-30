6d44e389ef5be8804be62118596692865c0cc2e6
Mukesh Kumar <mkso13189@gmail.com>
2024-10-30 15:13:10 +0530

    Remove BoardProvider context provider from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import Main from "./components/Main";
-import { BoardProvider } from "./components/BoardContext";
 
 export default function App() {
-  return (
-    <BoardProvider>
-      <Main />
-    </BoardProvider>
-  );
+  return <Main />;
 }
