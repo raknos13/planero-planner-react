160790a0048aebbdea3ef14044b9beb7df9a293e
Mukesh Kumar <mkso13189@gmail.com>
2024-11-03 22:04:16 +0530

    Use callButtonRef to close popover on blur

--- src/components/ListOptionsPopover.jsx
+++ src/components/ListOptionsPopover.jsx
@@ -1,51 +1,62 @@
 import { useRef, useEffect } from "react";
 import { FiX } from "react-icons/fi";
 
-const ListOptionsPopover = ({ isOpen, onClose, onEdit, onDelete }) => {
+const ListOptionsPopover = ({
+  isOpen,
+  onClose,
+  onEdit,
+  onDelete,
+  callButtonRef,
+}) => {
   const popoverRef = useRef(null);
 
   useEffect(() => {
     function handleClickOutside(event) {
-      if (!popoverRef && !popoverRef.current.contains(event.target)) {
+      if (
+        popoverRef.current &&
+        !popoverRef.current.contains(event.target) &&
+        !callButtonRef.current.contains(event.target)
+      ) {
         onClose();
       }
     }
+
     if (isOpen) {
       document.addEventListener("mousedown", handleClickOutside);
       return () =>
         document.removeEventListener("mousedown", handleClickOutside);
     }
-  }, [isOpen, onClose]);
+  }, [isOpen, onClose, callButtonRef]);
 
   if (!isOpen) return null;
 
   return (
     <div
       ref={popoverRef}
-      className="flex flex-col top-10 left-16 absolute z-50 w-48 h-32 text-sm mb-2 rounded-md shadow-lg border border-gray-300 bg-white"
+      className="top-9 left-28 absolute z-50 w-36 h-32 text-sm mb-2 rounded-md shadow-lg border border-gray-300 bg-white"
     >
-      <div className="flex justify-between p-3">
-        <h2 className="font-semibold">List options</h2>
+      <div className="flex justify-between items-center p-3">
+        <h2 className="font-semibold text-gray-700">List options</h2>
         <button
           onClick={onClose}
-          className="rounded-full p-1 hover:bg-gray-300"
+          className="rounded-full p-1 text-gray-400 hover:text-gray-600 hover:bg-gray-200"
         >
           <FiX />
         </button>
       </div>
-      <div className="flex flex-col items-start">
+      <div>
         <button
           onClick={() => {
             onEdit();
             onClose();
           }}
-          className="w-full h-8 hover:bg-gray-100 px-4"
+          className="w-full h-8 hover:bg-gray-100 px-4 text-left"
         >
           Edit
         </button>
         <button
           onClick={onDelete}
-          className="w-full h-8 hover:bg-gray-100 px-4"
+          className="w-full h-8 hover:bg-gray-100 px-4 text-left"
         >
           Delete
         </button>
