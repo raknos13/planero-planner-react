58e9b95f1482bf27aa83cad0c6367834725c5bdd
Mukesh Kumar <mkso13189@gmail.com>
2024-11-06 19:50:46 +0530

    Format tailwind classes

--- src/components/Board/EmptyBoard.jsx
+++ src/components/Board/EmptyBoard.jsx
@@ -34,8 +34,8 @@ export const EmptyBoard = () => {
       <div className="relative w-44">
         <button
           className="flex items-center cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg
-border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
-active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
+                     border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
+                     active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
           style={{ position: "absolute", top: 0 }}
           onClick={() => setShowBoardCreator(true)}
           ref={buttonRef}
