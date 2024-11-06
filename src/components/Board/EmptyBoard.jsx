216cb235ff44b11ec55c6a61840a94bab3b1430b
Mukesh Kumar <mkso13189@gmail.com>
2024-11-06 16:05:37 +0530

    Design empty board area

--- src/components/Board/EmptyBoard.jsx
+++ src/components/Board/EmptyBoard.jsx
@@ -1,15 +1,27 @@
+import notFound from "../../assets/not-found.svg";
+import { FiPlus } from "react-icons/fi";
+
 export const EmptyBoard = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-4 w-full">
-      <div>No boards present. Click to add new board</div>
-      <button
-        // className="p-3 bg-green-400 hover:bg-green-500 rounded-lg"
-        className="cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg
+    <div className="flex flex-col justify-start items-center w-full space-y-4 px-4 overflow-x-auto">
+      <figure>
+        <img src={notFound} alt="Not found svg" className="h-64 mt-10" />
+      </figure>
+      <div className="flex flex-col items-center text-gray-700 text-lg">
+        <p>Uh-uh! No boards present</p>
+        <p> Click to add a new board</p>
+      </div>
+      <div className="relative w-44">
+        <button
+          className="flex items-center cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg
 border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
 active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
-      >
-        Add New Board
-      </button>
+          style={{ position: "absolute", top: 0 }}
+        >
+          <FiPlus size={20} />
+          Add New Board
+        </button>
+      </div>
     </div>
   );
 };
