a9832a8bd2ee22c049fbeaac2489b3c36c8ff190
Mukesh Kumar <mkso13189@gmail.com>
2024-11-06 19:50:27 +0530

    Change github icon

--- src/components/shared/GithubButton.jsx
+++ src/components/shared/GithubButton.jsx
@@ -1,10 +1,12 @@
-import { FiGithub } from "react-icons/fi";
+// import { FiGithub } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa6";
+
 export const GithubButton = ({ link }) => {
   return (
     <a href={link} target="_blank">
       <div className="flex justify-center items-center gap-1 bg-gray-300 p-2 hover:bg-gray-700 hover:text-white transition-all duration-200 ease-linear">
         <button>
-          <FiGithub size={16} />
+          <FaGithub size={16} />
         </button>
         <span className="text-sm">View on GitHub</span>
       </div>
