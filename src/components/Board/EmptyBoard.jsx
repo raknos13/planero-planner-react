b344a05be24a7c9bbc05313881c6f0fbc248e74d
Mukesh Kumar <mkso13189@gmail.com>
2024-11-06 17:33:59 +0530

    Fix #23: Add new board popover and position it

--- src/components/Board/EmptyBoard.jsx
+++ src/components/Board/EmptyBoard.jsx
@@ -1,12 +1,32 @@
 import notFound from "../../assets/not-found.svg";
 import { FiPlus } from "react-icons/fi";
+import { BoardCreatorPopover } from "../shared";
+import { useState, useRef } from "react";
+import { useBoardContext } from "./";
 
 export const EmptyBoard = () => {
+  const { addNewBoard } = useBoardContext();
+  const [showBoardCreator, setShowBoardCreator] = useState(false);
+  const buttonRef = useRef(null);
+
+  function handleCreateBoard(title, color) {
+    addNewBoard(title, color);
+  }
+
   return (
-    <div className="flex flex-col justify-start items-center w-full space-y-4 px-4 overflow-x-auto">
+    <div className="relative flex flex-col justify-start items-center w-svw space-y-4 px-4 overflow-x-auto">
       <figure>
         <img src={notFound} alt="Not found svg" className="h-64 mt-10" />
       </figure>
+      <div className="relative right-32 bottom-44">
+        {showBoardCreator && (
+          <BoardCreatorPopover
+            onClose={() => setShowBoardCreator(false)}
+            onCreateBoard={handleCreateBoard}
+            buttonRef={buttonRef}
+          />
+        )}
+      </div>
       <div className="flex flex-col items-center text-gray-700 text-lg">
         <p>Uh-uh! No boards present</p>
         <p> Click to add a new board</p>
@@ -17,6 +37,8 @@ export const EmptyBoard = () => {
 border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
 active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
           style={{ position: "absolute", top: 0 }}
+          onClick={() => setShowBoardCreator(true)}
+          ref={buttonRef}
         >
           <FiPlus size={20} />
           Add New Board
