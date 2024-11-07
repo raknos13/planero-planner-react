727be4f55d9c19184c370df35464cfc0361e1e97
Mukesh Kumar <mkso13189@gmail.com>
2024-11-07 16:08:13 +0530

    Create DarkModeToggle component

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -2,3 +2,4 @@ export { BoardCreatorPopover } from "./BoardCreatorPopover";
 export { MoreOptionsPopover } from "./MoreOptionsPopover";
 export { GithubButton } from "./GithubButton";
 export { AutoResizeTextarea } from "./AutoResizeTextarea";
+export { DarkModeToggle } from "./DarkModeToggle";
