695aa88e7d87520b5d8325ad6714ca7905bc1bd0
Mukesh Kumar <mkso13189@gmail.com>
2024-11-07 16:07:56 +0530

    Define generic color classes for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "rgba(var(--color-bg))",
+        primary: "rgba(var(--color-primary))",
+        secondary: "rgba(var(--color-secondary))",
+        text: "rgba(var(--color-text))",
+        hover: "rgba(var(--color-hover))",
+        border: "rgba(var(--color-border))",
+      },
+    },
   },
   plugins: [],
 };
