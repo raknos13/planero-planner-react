6cc3c650f9219d28a13ee5f2ec7d0bf0bdc92bde
Mukesh Kumar <mkso13189@gmail.com>
2024-11-07 16:21:42 +0530

    Add logo icon in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
-import { FaUserCircle } from "react-icons/fa";
+import { FaUserCircle, FaTrello } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
 import { DarkModeToggle } from "./shared/DarkModeToggle";
 
 const Header = () => {
   return (
     <div className="flex justify-between items-center px-3 py-0.5 bg-background text-text border-b border-border z-30">
-      <h1 className="text-md font-bold text-center">Planero project manager</h1>
+      <div className="flex items-center gap-2">
+        <FaTrello size={20} />
+        <h1 className="text-md font-bold text-center">
+          Planero project manager
+        </h1>
+      </div>
       <section className="flex justify-evenly items-center gap-4">
         <a
           href="https://github.com/raknos13/planero-trello-clone-react"
