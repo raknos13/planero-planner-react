38aaec5244e631438e02fc890ce6798401c7fb36
Mukesh Kumar <mkso13189@gmail.com>
2024-11-07 21:43:57 +0530

    Wrap app in theme context

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,20 @@
-import { BoardProvider, SidebarProvider } from "../contexts";
+import { BoardProvider, SidebarProvider, ThemeProvider } from "../contexts";
 import { Header, Board, Sidebar } from "./";
 
 export default function Main() {
   return (
     <BoardProvider>
-      <div className="h-screen w-screen overflow-hidden flex flex-col">
-        <Header />
-        <div className="flex flex-grow">
-          <SidebarProvider>
-            <Sidebar />
-          </SidebarProvider>
-          <Board />
+      <ThemeProvider>
+        <div className="h-screen w-screen overflow-hidden flex flex-col">
+          <Header />
+          <div className="flex flex-grow">
+            <SidebarProvider>
+              <Sidebar />
+            </SidebarProvider>
+            <Board />
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     </BoardProvider>
   );
 }
