4c879c2ec32124dc7fd4de5fd7b8d12917ca28fc
Mukesh Kumar <mkso13189@gmail.com>
2024-11-07 21:43:40 +0530

    Create theme context

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,15 @@
+import { useState, createContext, useContext } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <ThemeContext.Provider value={(theme, setTheme)}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
