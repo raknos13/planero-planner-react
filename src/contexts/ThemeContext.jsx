2f1f38872ee1faeda8cf05bdfacc69859d4e8be2
Mukesh Kumar <mkso13189@gmail.com>
2024-11-07 22:25:09 +0530

    Make dark theme default

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { useState, createContext, useContext } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
