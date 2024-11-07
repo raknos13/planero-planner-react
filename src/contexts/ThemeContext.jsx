b603345bd59878d1d05ab2d03596c3e1dd0aa1c8
Mukesh Kumar <mkso13189@gmail.com>
2024-11-07 21:51:30 +0530

    Fix typo in ThemeContext

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -6,7 +6,7 @@ export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("light");
 
   return (
-    <ThemeContext.Provider value={(theme, setTheme)}>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
       {children}
     </ThemeContext.Provider>
   );
