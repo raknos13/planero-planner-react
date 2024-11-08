1f0ffa276170c0c2f8541edbeebe4335f8adfa02
Mukesh Kumar <mkso13189@gmail.com>
2024-11-08 18:50:43 +0530

    Add shadcn ui configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,66 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  darkMode: ["class", "class"],
   theme: {
-    extend: {
-      colors: {
-        "bg-primary": "rgba(var(--color-bg-primary))",
-        "bg-secondary": "rgba(var(--color-bg-secondary))",
-        "bg-card": "rgba(var(--color-bg-card))",
-        "bg-card-hover": "rgba(var(--color-bg-card-hover))",
-        "text-primary": "rgba(var(--color-text-primary))",
-        "text-secondary": "rgba(var(--color-text-secondary))",
-        "bg-hover": "rgba(var(--color-hover))",
-        border: "rgba(var(--color-border))",
-        "border-hover": "rgba(var(--color-border-hover))",
-        accent: "rgba(var(--color-accent))",
-        "accent-hover": "rgba(var(--color-accent-hover))",
-      },
-    },
+  	extend: {
+  		colors: {
+  			'bg-primary': 'rgba(var(--color-bg-primary))',
+  			'bg-secondary': 'rgba(var(--color-bg-secondary))',
+  			'bg-card': 'rgba(var(--color-bg-card))',
+  			'bg-card-hover': 'rgba(var(--color-bg-card-hover))',
+  			'text-primary': 'rgba(var(--color-text-primary))',
+  			'text-secondary': 'rgba(var(--color-text-secondary))',
+  			'bg-hover': 'rgba(var(--color-hover))',
+  			border: 'hsl(var(--border))',
+  			'border-hover': 'rgba(var(--color-border-hover))',
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			'accent-hover': 'rgba(var(--color-accent-hover))',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
