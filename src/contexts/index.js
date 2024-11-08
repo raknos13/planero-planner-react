c4c8b18f4d44522f31223c406bd4f28ba7a6534d
Mukesh Kumar <mkso13189@gmail.com>
2024-11-08 19:29:50 +0530

    Add auth context export in barrel file

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,3 +1,4 @@
 export { SidebarProvider, useSidebar } from "./SidebarContext";
 export { BoardProvider, useBoardContext } from "./BoardContext";
 export { ThemeProvider, useTheme } from "./ThemeContext";
+export { AuthProvider, useAuth } from "./AuthContext";
