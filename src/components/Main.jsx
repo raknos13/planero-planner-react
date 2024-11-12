0a63a02ac91903c7386fdad4b10fe7b41e970fcb
Mukesh Kumar <mkso13189@gmail.com>
2024-11-12 18:31:53 +0530

    Add app routes using react-router-dom

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,24 @@
 import { SidebarProvider, useAuth } from "../contexts";
 import { Navbar, Board, Sidebar } from "./";
 import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import SignupPage from "./SignupPage";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const { isLoggedIn } = useAuth();
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+}
 
 export default function Main() {
   const { isLoading, isLoggedIn } = useAuth();
@@ -12,16 +30,41 @@ export default function Main() {
   return (
     <div className="h-screen w-screen overflow-hidden flex flex-col">
       <Navbar />
-      {isLoggedIn ? (
-        <div className="flex flex-grow">
-          <SidebarProvider>
-            <Sidebar />
-          </SidebarProvider>
-          <Board />
-        </div>
-      ) : (
-        <HomePage />
-      )}
+      {/* {isLoggedIn ? ( */}
+      {/*   <div className="flex flex-grow"> */}
+      {/*     <SidebarProvider> */}
+      {/*       <Sidebar /> */}
+      {/*     </SidebarProvider> */}
+      {/*     <Board /> */}
+      {/*   </div> */}
+      {/* ) : ( */}
+      {/*   <HomePage /> */}
+      {/* )} */}
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isLoggedIn ? (
+                <ProtectedRoute>
+                  <div className="flex flex-grow">
+                    <SidebarProvider>
+                      <Sidebar />
+                    </SidebarProvider>
+                    <Board />
+                  </div>
+                </ProtectedRoute>
+              ) : (
+                <HomePage />
+              )
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          {/* Catch-all route for 404 */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
