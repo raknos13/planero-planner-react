a13e48e56739c4d75ed4d68ca98c1ee9b9a71539
Mukesh Kumar <mkso13189@gmail.com>
2024-11-15 03:49:46 +0530

    Merge SidebarCollapsed into Sidebar add transition

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,8 @@
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiChevronRight } from "react-icons/fi";
 import { useState, useRef, useEffect } from "react";
 import { useBoardContext, useSidebar, useTheme } from "../../contexts/";
 import { GithubButton } from "../shared";
-import {
-  SidebarCollapsed,
-  SidebarHeader,
-  BoardList,
-  BoardListHeader,
-} from "./";
+import { SidebarHeader, BoardList, BoardListHeader } from "./";
 
 export const Sidebar = () => {
   const { boards, activeBoardId, addNewBoard, editBoard } = useBoardContext();
@@ -51,54 +46,64 @@ export const Sidebar = () => {
     setActivePopoverBoard(null);
   }
 
-  if (isCollapsed) {
-    return (
-      <SidebarCollapsed
-        isCollapsed={isCollapsed}
-        setIsCollapsed={setIsCollapsed}
-      />
-    );
-  }
-
   return (
     <div
-      className="z-20 flex-shrink-0 w-52 bg-black/50 backdrop-blur-sm text-white border-r border-border shadow-md"
+      // className="z-20 flex-shrink-0 w-52 backdrop-blur-sm text-white border-r border-border shadow-md"
+      className={`z-20 flex-shrink-0 ${isCollapsed ? "w-4" : "w-52"} text-white/70 backdrop-blur-sm border-r border-border shadow-md transition-width duration-300 ease-in-out`}
       style={{
         backgroundColor:
           theme === "dark" ? "hsla(0, 0%, 0%, 0.8)" : "hsla(0, 0%, 0%, 0.5)",
       }}
     >
-      <div className="flex flex-col justify-between h-full">
-        <SidebarHeader
-          isCollapsed={isCollapsed}
-          setIsCollapsed={setIsCollapsed}
-        />
-
-        <BoardListHeader
-          addButtonRef={addButtonRef}
-          handleCreateBoard={handleCreateBoard}
-        />
-
-        {activeBoardId ? (
-          <BoardList
-            boards={boards}
-            editedTitle={editedTitle}
-            setEditedTitle={setEditedTitle}
-            handleEdit={handleEdit}
-            handleTitleSubmit={handleTitleSubmit}
-            inputRef={inputRef}
-            showBoardCreator={showBoardCreator}
-            setShowBoardCreator={setShowBoardCreator}
-            handleCreateBoard={handleCreateBoard}
-            addButtonRef={addButtonRef}
-          />
-        ) : (
-          <EmptyBoardMessage />
-        )}
-        <div className="mt-auto">
-          <GithubButton link="https://github.com/mksonkar/planero-planner-react" />
+      {isCollapsed ? (
+        <div
+          className={`h-full flex-shrink-0 w-4 cursor-pointer`}
+          onClick={() => setIsCollapsed(false)}
+        >
+          <div className="mt-2">
+            <button
+              onClick={() => setIsCollapsed(!isCollapsed)}
+              className={`absolute z-30 opacity-100 text-gray-200 border-2
+            border-border p-1 rounded-full
+                ${theme === "dark" ? "bg-black/80 hover:bg-black/90" : "bg-black/50 hover:bg-black/60"}`}
+            >
+              <FiChevronRight size={16} />
+            </button>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex flex-col justify-between h-full">
+          <SidebarHeader
+            isCollapsed={isCollapsed}
+            setIsCollapsed={setIsCollapsed}
+          />
+
+          <BoardListHeader
+            addButtonRef={addButtonRef}
+            handleCreateBoard={handleCreateBoard}
+          />
+
+          {activeBoardId ? (
+            <BoardList
+              boards={boards}
+              editedTitle={editedTitle}
+              setEditedTitle={setEditedTitle}
+              handleEdit={handleEdit}
+              handleTitleSubmit={handleTitleSubmit}
+              inputRef={inputRef}
+              showBoardCreator={showBoardCreator}
+              setShowBoardCreator={setShowBoardCreator}
+              handleCreateBoard={handleCreateBoard}
+              addButtonRef={addButtonRef}
+            />
+          ) : (
+            <EmptyBoardMessage />
+          )}
+          <div className="mt-auto">
+            <GithubButton link="https://github.com/mksonkar/planero-planner-react" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
