d74a3d7eb3e0a00ee37357827002ed8c8dfe76fb
Mukesh Kumar <mkso13189@gmail.com>
2024-11-21 21:04:06 +0530

    Correct border color in dark mode

--- src/components/Sidebar/SidebarHeader.jsx
+++ src/components/Sidebar/SidebarHeader.jsx
@@ -1,20 +1,24 @@
-import { useAuth } from "@/contexts";
+import { useAuth, useBoardContext, useTheme } from "@/contexts";
 import { FiChevronLeft, FiHome } from "react-icons/fi";
 
 export const SidebarHeader = ({ isCollapsed, setIsCollapsed }) => {
   const { currentUser } = useAuth();
+  const { activeBoardId } = useBoardContext();
+  const { theme } = useTheme();
 
   return (
-    <div className="flex justify-between items-center px-3 py-2 border-b border-border">
-      <div className="flex gap-2 items-center">
-        <FiHome size={15} />
-        <h1 className="font-bold text-xs">
-          {currentUser.displayName || "User"} &apos;s workspace
+    <div
+      className={`flex items-center justify-between border-b px-3 py-2 ${activeBoardId ? "border-border" : theme === "light" ? "border-gray-300" : "border-border"}`}
+    >
+      <div className="flex items-center gap-2">
+        <FiHome size={20} />
+        <h1 className="text-sm font-bold">
+          {currentUser.displayName || "User's"} workspace
         </h1>
       </div>
       <button
         onClick={() => setIsCollapsed(!isCollapsed)}
-        className="hover:bg-white/30 p-1 rounded-md"
+        className="rounded-md p-1 hover:bg-white/30"
       >
         <FiChevronLeft size={18} />
       </button>
